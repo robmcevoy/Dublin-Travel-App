4a4bb4219a8e20f14b4bf0cfabcf85788cf75e45
Rob McEvoy <robert.mcevoy6@mail.dcu.ie>
2015-02-05 19:18:06 +0000

    Clickable Hompage icons

--- DublinTravel/src/com/example/dublintravel/Homepage.java
+++ DublinTravel/src/com/example/dublintravel/Homepage.java
@@ -2,7 +2,10 @@ package com.example.dublintravel;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Homepage extends Activity {
 
@@ -10,6 +13,51 @@ public class Homepage extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_homepage);
+		final ImageView dublinBusImageView = (ImageView) findViewById(R.id.dublinBusLogo);
+	    final DublinBusOperator dublinBusOperator = new DublinBusOperator();
+	    final ImageView irishRailImageView = (ImageView) findViewById(R.id.irishRailLogo);
+	    final RtpiXmlOperator irishRailOperator = new RtpiXmlOperator();
+	    final BusEireannOperator busEireannOperator = new BusEireannOperator();
+	    final ImageView busEireannImageView = (ImageView) findViewById(R.id.busEireannLogo);
+	    final LuasOperator luasOperator = new LuasOperator();
+	    final ImageView luasImageView = (ImageView) findViewById(R.id.luasLogo);
+	    final ImageView carsImageView = (ImageView) findViewById(R.id.carsLogo);
+	    
+	    dublinBusImageView.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+            	Intent i = new Intent(getApplicationContext(), RtpiDashboard.class);
+            	startActivity(i);
+            }
+        });
+	    
+	    luasImageView.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+            	Intent i = new Intent(getApplicationContext(), RtpiDashboard.class);
+            	startActivity(i);
+            }
+        });
+	    
+	    irishRailImageView.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {	
+            	Intent i = new Intent(getApplicationContext(), RtpiDashboard.class);
+            	startActivity(i);
+            }
+        });
+	    
+	    busEireannImageView.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+            	Intent i = new Intent(getApplicationContext(), RtpiDashboard.class);
+            	startActivity(i);
+            }
+        });
 	}
 
 	@Override

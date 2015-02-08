9d16416412585029aae0792ca39da6ce7872d815
Rob McEvoy <robert.mcevoy6@mail.dcu.ie>
2015-02-08 18:09:48 +0000

    Ensure due times are returned in the same format

--- DublinTravel/src/com/example/dublintravel/JSONParser.java
+++ DublinTravel/src/com/example/dublintravel/JSONParser.java
@@ -1,9 +1,14 @@
 package com.example.dublintravel;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
 
 //used as a helper to parse JSON format web service responses
 public class JSONParser implements Parser {
@@ -29,7 +34,7 @@ public class JSONParser implements Parser {
 				resultsLength = result.length();
 				for(int i=0; ((i<resultsLength) && (i<MAX_NUM_RESULTS)); i++){
 					JSONObject resultItem = result.getJSONObject(i);
-					duetime = resultItem.getString("duetime");
+					duetime = convertToTimeFormat(resultItem.getString("duetime"));
 					destination = resultItem.getString("destination");
 					route = resultItem.getString("route");
 					stopInfo = new StopInfo(route, destination, duetime);
@@ -49,6 +54,30 @@ public class JSONParser implements Parser {
 			stopInfo.setErrorMessage(errorMessage);
 			stopInfoArray.add(stopInfo);
 			return stopInfoArray;
+		}
+	}
+	
+	public String convertToTimeFormat(String duetime){
+		
+		if(!duetime.contains(":")){
+			return duetime;
+		}
+		else{
+			SimpleDateFormat format = new SimpleDateFormat("HH:mm");
+			Date d1;
+			Date now = new Date();
+			long difference=0;
+			String tmp;
+			try {
+				d1 = format.parse(duetime);
+				tmp = format.format(now);
+				now = format.parse(tmp);
+				difference = d1.getTime() - now.getTime();
+				difference = difference / (60 * 1000) % 60;
+				
+			} catch (ParseException e) {
+			}
+			return Long.toString(difference);
 		}
 	}
 

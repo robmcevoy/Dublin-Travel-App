c115373048de61d2f4be4e19d14711fb2271eb35
Rob McEvoy <robert.mcevoy6@mail.dcu.ie>
2015-02-24 16:26:14 +0000

    Link now not clickable within Twitter Feed
    Fix for https://github.com/robmcevoy/Dublin-Travel-App/issues/3

--- DublinTravel/src/com/example/dublintravel/EmbeddedBrowser.java
+++ DublinTravel/src/com/example/dublintravel/EmbeddedBrowser.java
@@ -2,14 +2,13 @@ package com.example.dublintravel;
 
 import android.os.Build;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
 
 public class EmbeddedBrowser {
 	
 	private WebView webview;
 	private RtpiController rtpiController;
 	private WebviewInterface webviewInterface;
-	protected String url = "file:///android_asset/twitter_feed/twitter_feed.html";
+	protected String url = "";
 	
 	public EmbeddedBrowser(WebView webview, RtpiController rtpiController){
 		this.webview = webview;
@@ -19,17 +18,10 @@ public class EmbeddedBrowser {
 	
 	public void start(){
 		webview.getSettings().setJavaScriptEnabled(true);
+		webview.getSettings().setSupportMultipleWindows(true);
 		webview.addJavascriptInterface(webviewInterface, webviewInterface.getInterfaceName());
 		webview.loadUrl(url);
-		webview.setWebViewClient(new WebViewClient() {
-
-	        public void onReceivedError(WebView view, int errorCode,
-	                String description, String failingUrl) {}
-
-	        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-	            view.loadUrl(url);
-	            return true;
-	    }});
+	    
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
 			WebView.setWebContentsDebuggingEnabled(true);
 		}
